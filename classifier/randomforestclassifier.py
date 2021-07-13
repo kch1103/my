46dce507884fe193ad5a31d8223833c4ea43a58b
kch1103 <shipshark72@gmail.com>
2021-07-13 19:43:05 +0900

    Create randomforestclassifier.py

---
+++ classifier/randomforestclassifier.py
@@ -0,0 +1,41 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestClassifier 
+import gzip
+
+#load data 
+with gzip.open('C:/Users/USER/Documents/git/my/data/train-labels-idx1-ubyte (1).gz', 'rb') as lbpath:
+    y_train = np.frombuffer(lbpath.read(), np.uint8, offset = 8)
+
+
+with gzip.open('C:/Users/USER/Documents/git/my/data/train-images-idx3-ubyte (1).gz', 'rb') as imgpath:
+    x_train = np.frombuffer(imgpath.read(), np.uint8, offset = 16).reshape(len(y_train), 28, 28)
+
+with gzip.open('C:/Users/USER/Documents/git/my/data/t10k-labels-idx1-ubyte (1).gz', 'rb') as lbpath:
+    y_test = np.frombuffer(lbpath.read(), np.uint8, offset = 8)
+
+with gzip.open('C:/Users/USER/Documents/git/my/data/t10k-images-idx3-ubyte (1).gz', 'rb') as imgpath:
+    x_test = np.frombuffer(imgpath.read(), np.uint8, offset = 16).reshape(len(y_test), 28, 28)
+
+
+labels = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+#3D array to 2D array 
+x_train = x_train.reshape(len(y_train), 28 * 28)
+x_test = x_test.reshape(len(y_test), 28 * 28)
+
+#create model & train
+model = RandomForestClassifier(n_estimators = 100, max_depth = 5, random_state = 0)
+model.fit(x_train, y_train)
+
+y_pred = model.predict(x_test)
+
+accuracy_score = model.score(x_test, y_test)
+print(accuracy_score)
+
+x_test = x_test.reshape(len(y_test), 28, 28)
+
+for i in range(10):
+    plt.imshow(x_test[i], cmap = plt.cm.get_cmap("binary"))
+    print("Predict: ", y_pred[i])
+    plt.imshow()
